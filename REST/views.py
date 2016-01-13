bed287b4012778b8b13266586a2e020a1b2276a3
Joshua Campbell <joshua.campbell@demonware.net>
2016-01-13 11:03:52 -0800

    Merge branch 'backup2'

--- REST/views.py
+++ REST/views.py
@@ -25,6 +25,7 @@ def retrieveOrCreateNewTitle(request, format=None):
 	cursor = db.cursor()
 	#data = cursor.fetchone()
 	#print("Database version : %s " % data)
+	print("hello world")
 	
 	if request.method == 'GET':
 		cursor.execute("SELECT * FROM CallOfDuty;")
